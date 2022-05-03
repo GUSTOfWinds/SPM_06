27569c40f1ec776f088ac3e4e80e8fc43e162d9e
Jonarn <jonathan.arnfjell@gmail.com>
2022-05-03 18:14:07 +0200

    Ny HP, Stamina och hotbar

--- Viking Attack/Assets/Scripts/Character_scripts/Player/ToggleCharacterScreen.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/Player/ToggleCharacterScreen.cs
@@ -6,20 +6,33 @@ using UnityEngine.InputSystem;
 public class ToggleCharacterScreen : MonoBehaviour
 {
     [SerializeField] private GameObject characterScreen;
+<<<<<<< HEAD
+=======
+    public Animator animator;
+    //[SerializeField] private InputSystem inputSystem;
+>>>>>>> parent of d094a905 (Revert "Ny HP, Stamina och hotbar. WIP. Animationer för character screen. Finare knappar.")
     public void ToggleScreen()
     {
-        if (characterScreen.active)
+        if (animator.GetBool("CSSheetOpen"))
         {
+<<<<<<< HEAD
             gameObject.GetComponent<ToggleMenu>().enabled = true;
+=======
+            animator.SetBool("CSSheetOpen", false);
+>>>>>>> parent of d094a905 (Revert "Ny HP, Stamina och hotbar. WIP. Animationer för character screen. Finare knappar.")
             Cursor.lockState = CursorLockMode.Locked;
-            characterScreen.SetActive(false);
+            
             
         }
         else
         {
+<<<<<<< HEAD
             gameObject.GetComponent<ToggleMenu>().enabled = false;
+=======
+            animator.SetBool("CSSheetOpen", true);            
+>>>>>>> parent of d094a905 (Revert "Ny HP, Stamina och hotbar. WIP. Animationer för character screen. Finare knappar.")
             Cursor.lockState = CursorLockMode.None;
-            characterScreen.SetActive(true);
+            
             characterScreen.GetComponent<CharacterScreen>().OpenCharacterScreen();
         }
     }
