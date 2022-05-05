f1e4baa80bc14e534936a132da2d803798346eb4
ryokz <82468014+ryokz@users.noreply.github.com>
2022-05-05 18:23:54 +0200

    Revert "+ player animations"
    This reverts commit c41fab0811039d838c7bab4f7b40f0e44f454128.

--- Viking Attack/Assets/Scripts/Inventory_scripts/ItemSwordBehavior.cs
+++ Viking Attack/Assets/Scripts/Inventory_scripts/ItemSwordBehavior.cs
@@ -5,25 +5,20 @@ using ItemNamespace;
 
 public class ItemSwordBehavior : ItemBaseBehavior
 {
-    private Animator animator;
     private Camera mainCamera = null;
     private GlobalPlayerInfo globalPlayerInfo;
     private RaycastHit hit;
-    private bool canAttack = true;
 
 
     public void Awake()
     {
         mainCamera = GameObject.FindGameObjectWithTag("CameraMain").GetComponent<Camera>();
         globalPlayerInfo = gameObject.GetComponent<GlobalPlayerInfo>();
-        animator = gameObject.transform.Find("Prefab_PlayerBot").GetComponent<Animator>();
     }
     public override void Use()
     {       
-        // Checks if the player has enough stamina to attack, will then attack.
-        if (globalPlayerInfo.GetStamina() > belongingTo.GetStamina && canAttack)
+        if (Physics.SphereCast(mainCamera.transform.position, 1f,mainCamera.transform.forward, out hit, belongingTo.GetRange,LayerMask.GetMask("Enemy")))
         {
-<<<<<<< Updated upstream
             // Checks if the player has enough stamina to attack, will then attack.
             if (globalPlayerInfo.GetStamina() > belongingTo.GetStamina)
             {
@@ -31,27 +26,6 @@ public class ItemSwordBehavior : ItemBaseBehavior
                 hit.collider.gameObject.GetComponent<EnemyVitalController>().UpdateHealth(-belongingTo.GetDamage);
                 // ADD SWING ANIMATION HERE
             }
-=======
-            
-            globalPlayerInfo.UpdateStamina(-belongingTo.GetStamina);
-            // ADD SWING ANIMATION HERE
-            canAttack = false;
-            animator.Play("SwordAttack",animator.GetLayerIndex("Sword Attack"),0f);
-            animator.SetLayerWeight(animator.GetLayerIndex("Sword Attack"),1);
-            StartCoroutine(WaitToAttack(animator.GetCurrentAnimatorStateInfo(animator.GetLayerIndex("Sword Attack")).length/animator.GetCurrentAnimatorStateInfo(animator.GetLayerIndex("Sword Attack")).speed));
->>>>>>> Stashed changes
         }
     }
-    IEnumerator WaitToAttack(float time)
-    {
-
-        yield return new WaitForSeconds(time);
-        if(Physics.SphereCast(mainCamera.transform.position, 1f,mainCamera.transform.forward, out hit, belongingTo.GetRange,LayerMask.GetMask("Enemy")))
-                hit.collider.gameObject.GetComponent<EnemyVitalController>().CmdUpdateHealth(-belongingTo.GetDamage);
-        animator.SetLayerWeight(animator.GetLayerIndex("Sword Attack"),0);
-        canAttack = true;
-        
-    }
 }
-        
-    
