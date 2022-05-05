eb9c4d19786de17596e87bb85fcb8030b0e5e767
ryokz <82468014+ryokz@users.noreply.github.com>
2022-05-05 18:20:29 +0200

    Player animations

--- Viking Attack/Assets/Scripts/Player_movement_camera_scripts/Camera/CameraMovement3D.cs
+++ Viking Attack/Assets/Scripts/Player_movement_camera_scripts/Camera/CameraMovement3D.cs
@@ -8,7 +8,6 @@ using Camera = UnityEngine.Camera;
 public class CameraMovement3D : NetworkBehaviour
 {
     [SerializeField]private GameObject firstPersonPosition;
-    [SerializeField]private GameObject thirdPersonPosition;
     [SerializeField] float mouseSensitivity = 1;
     [SerializeField] private bool lockMouse;
     private float rotationX;
@@ -28,15 +27,7 @@ public class CameraMovement3D : NetworkBehaviour
         if (mainCamera != null)
         {
             mainCamera.transform.SetParent(transform);
-            if (GetComponentInParent<PlayerScript3D>().firstPerson)
-            {
-                cameraPosition = firstPersonPosition.transform.localPosition;
-            }
-            else
-            {
-                cameraPosition = thirdPersonPosition.transform.localPosition;
-
-            }
+            mainCamera.transform.position = firstPersonPosition.transform.position;
         }
 
         if (isLocalPlayer)
@@ -49,29 +40,12 @@ public class CameraMovement3D : NetworkBehaviour
     {
         if (mainCamera != null)
         {
-            
             mainCamera.transform.SetParent(null);
             SceneManager.MoveGameObjectToScene(mainCamera.gameObject, SceneManager.GetActiveScene());
             mainCamera.orthographic = true;
             mainCamera.transform.localPosition = new Vector3(0f, 70f, 0f);
             mainCamera.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
 
-        }
-    }
-
-    void LateUpdate()
-    {
-        if (!isLocalPlayer) return;
-        //Looks if camera hits a collider and if it's true change position of camera to hit position
-        Vector3 cameraOffset = mainCamera.transform.rotation * cameraPosition;
-        RaycastHit hit;
-        if (Physics.SphereCast(mainCamera.transform.parent.transform.position, 0.1f, cameraOffset, out hit, cameraOffset.magnitude, ~(1 << mainCamera.transform.parent.gameObject.layer)))
-        {
-            mainCamera.transform.position = mainCamera.transform.parent.transform.position + cameraOffset.normalized * hit.distance;
-        }
-        else
-        {
-            mainCamera.transform.position = mainCamera.transform.parent.transform.position + cameraOffset;
         }
     }
     public void OnMouseMovement(InputAction.CallbackContext value)
--- Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerState.cs
+++ Viking Attack/Assets/Scripts/Player_movement_camera_scripts/PlayerState.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 //Base class to all player states
 public abstract class PlayerState : State
 {
+    private Animator animator;
     private PlayerScript3D player;
     //Returns player if player is null sets player veribal to owner veribal from State class 
     public PlayerScript3D Player => player = player ?? (PlayerScript3D)owner;
+    public Animator Animator => animator = animator ?? player.animator;
 }
