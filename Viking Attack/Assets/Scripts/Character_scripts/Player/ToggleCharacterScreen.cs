73ea67961b0f0aa746fbb860926bb3dc33a2c51a
munchosaurus <martinkings1@live.se>
2022-05-06 12:56:52 +0200

    Locking camera movement when character screen is open

--- Viking Attack/Assets/Scripts/Character_scripts/Player/ToggleCharacterScreen.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/Player/ToggleCharacterScreen.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class ToggleCharacterScreen : MonoBehaviour
 {
     [SerializeField] private GameObject characterScreen;
+    
     public Animator animator;
 
     public void ToggleScreen()
@@ -13,14 +14,20 @@ public class ToggleCharacterScreen : MonoBehaviour
         if (animator.GetBool("CSOpen"))
         {
             animator.SetBool("CSOpen", false);
-
+            if (gameObject.GetComponent<CameraMovement3D>().shouldBeLocked == false)
+            {
+                gameObject.GetComponent<CameraMovement3D>().shouldBeLocked = true;
+            }
             Cursor.lockState = CursorLockMode.Locked;
         }
         else
         {
             animator.SetBool("CSOpen", true);
             Cursor.lockState = CursorLockMode.None;
-            
+            if (gameObject.GetComponent<CameraMovement3D>().shouldBeLocked == true)
+            {
+                gameObject.GetComponent<CameraMovement3D>().shouldBeLocked = false;
+            }
             characterScreen.GetComponent<CharacterScreen>().OpenCharacterScreen();
         }
     }
