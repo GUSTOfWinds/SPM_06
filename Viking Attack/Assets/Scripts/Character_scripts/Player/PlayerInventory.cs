fde8ee8fb226d4695ab7b88048cb5eea0d85ec4d
munchosaurus <martinkings1@live.se>
2022-05-09 16:38:00 +0200

    Inventory updates

--- Viking Attack/Assets/Scripts/Character_scripts/Player/PlayerInventory.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/Player/PlayerInventory.cs
@@ -3,6 +3,7 @@ using System.Net.Mime;
 using Event;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace ItemNamespace
@@ -12,6 +13,7 @@ namespace ItemNamespace
         [SerializeField] private ItemBase[] inventory;
         [SerializeField] private GameObject[] sprites;
         [SerializeField] private Guid itemPickupGuid;
+        [SerializeField] private GameObject selectedItem;
         
 
         private void Start()
@@ -32,6 +34,8 @@ namespace ItemNamespace
                     
                     ItemBase.WeaponType weaponType = playerItemPickupEventInfo.itemBase.GetWeaponType;
                     
+                    
+                    // TODO ADD THAT HELD ITEM IS UPDATED TO THE WEAPON THAT IS PICKED UP
                     switch (weaponType)
                     {
                         case ItemBase.WeaponType.Sword:
@@ -39,6 +43,8 @@ namespace ItemNamespace
                             sprites[0].SetActive(true);
                             sprites[0].GetComponent<Image>().sprite = inventory[0].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>().SetItemSlot(0, inventory[0]); // sets the info in globalplayerinfo
+                            selectedItem.transform.position = sprites[0].transform.position;
+                            gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[0];
                             break;
 
                         case ItemBase.WeaponType.Spear:
@@ -46,6 +52,8 @@ namespace ItemNamespace
                             sprites[1].SetActive(true);
                             sprites[1].GetComponent<Image>().sprite = inventory[1].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>().SetItemSlot(1, inventory[1]); // sets the info in globalplayerinfo
+                            selectedItem.transform.position = sprites[1].transform.position;
+                            gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[1];
                             break;
                         
                         case ItemBase.WeaponType.Dagger:
@@ -53,9 +61,10 @@ namespace ItemNamespace
                             sprites[2].SetActive(true);
                             sprites[2].GetComponent<Image>().sprite = inventory[2].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>().SetItemSlot(2, inventory[2]); // sets the info in globalplayerinfo
+                            selectedItem.transform.position = sprites[2].transform.position;
+                            gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[2];
                             break;
                     }
-                    // code block
                     break;
                 // END OF INNER WEAPON SWITCH
                 
@@ -71,10 +80,40 @@ namespace ItemNamespace
             }
         }
 
-        // void ToggleItem(int slot)
-        // {
-        //     gameObject
-        // }
+        public void ToggleSword(InputAction.CallbackContext value)
+        {
+            if (inventory[0] != null)
+            {
+                gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[0];
+                selectedItem.transform.position = sprites[0].transform.position;
+            }
+            
+        }
+        public void ToggleSpear(InputAction.CallbackContext value)
+        {
+            if (inventory[1] != null)
+            {
+                gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[1];
+                selectedItem.transform.position = sprites[1].transform.position;
+            }
+        }
+        public void ToggleDagger(InputAction.CallbackContext value)
+        {
+            if (inventory[2] != null)
+            {
+                gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[2];
+                selectedItem.transform.position = sprites[2].transform.position;
+            }
+        }
+        public void ToggleFood(InputAction.CallbackContext value)
+        {
+            if (inventory[3] != null)
+            {
+                gameObject.GetComponent<PlayerItemUsageController>().itemBase = inventory[3];
+                selectedItem.transform.position = sprites[3].transform.position;
+            }
+        }
+        
         
     }
 }
