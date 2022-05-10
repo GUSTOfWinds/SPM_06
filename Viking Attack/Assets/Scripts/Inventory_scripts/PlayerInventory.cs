0e8c5acf52c077a5cd41ea02f03b20eb737cfdd8
Jonarn <jonathan.arnfjell@gmail.com>
2022-05-10 15:12:22 +0200

    Sicko Mode animationer

--- Viking Attack/Assets/Scripts/Inventory_scripts/PlayerInventory.cs
+++ Viking Attack/Assets/Scripts/Inventory_scripts/PlayerInventory.cs
@@ -13,6 +13,8 @@ namespace ItemNamespace
         [SerializeField] private Guid itemPickupGuid;
         [SerializeField] private GameObject selectedItem;
         [SerializeField] private GameObject meatStackNumber;
+        [SerializeField] public Animator animator;
+
 
 
         private void Start()
@@ -29,7 +31,7 @@ namespace ItemNamespace
             switch (playerItemPickupEventInfo.itemBase.GetItemType)
             {
                 case ItemBase.ItemType.Weapon:
-
+                    animator.SetTrigger("itemPOPUP");
                     switch (playerItemPickupEventInfo.itemBase.GetWeaponType)
                     {
                         // Sets the inventory slot, sprite updates globalplayerinfo, what item the player is using 
@@ -40,7 +42,7 @@ namespace ItemNamespace
                             sprites[0].GetComponent<Image>().sprite = inventory[0].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>()
                                 .SetItemSlot(0, inventory[0]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[0].transform.position;
+                            selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f, 40f, 0f);
                             gameObject.GetComponent<PlayerItemUsageController>()
                                 .ChangeItem(playerItemPickupEventInfo.itemBase);
                             break;
@@ -51,7 +53,7 @@ namespace ItemNamespace
                             sprites[1].GetComponent<Image>().sprite = inventory[1].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>()
                                 .SetItemSlot(1, inventory[1]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[1].transform.position;
+                            selectedItem.transform.position = sprites[1].transform.position + new Vector3(0f, 40f, 0f);
                             gameObject.GetComponent<PlayerItemUsageController>()
                                 .ChangeItem(playerItemPickupEventInfo.itemBase);
                             break;
@@ -62,7 +64,7 @@ namespace ItemNamespace
                             sprites[2].GetComponent<Image>().sprite = inventory[2].GetSprite;
                             gameObject.GetComponent<GlobalPlayerInfo>()
                                 .SetItemSlot(2, inventory[2]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[2].transform.position;
+                            selectedItem.transform.position = sprites[2].transform.position + new Vector3(0f, 40f, 0f);
                             gameObject.GetComponent<PlayerItemUsageController>()
                                 .ChangeItem(playerItemPickupEventInfo.itemBase);
                             break;
@@ -101,7 +103,8 @@ namespace ItemNamespace
             if (inventory[0] != null)
             {
                 gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[0]);
-                selectedItem.transform.position = sprites[0].transform.position;
+                selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f,40f,0f);
+                animator.SetTrigger("itemPOPUP");
             }
         }
 
@@ -110,7 +113,8 @@ namespace ItemNamespace
             if (inventory[1] != null)
             {
                 gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[1]);
-                selectedItem.transform.position = sprites[1].transform.position;
+                selectedItem.transform.position = sprites[1].transform.position + new Vector3(0f, 40f, 0f);
+                animator.SetTrigger("itemPOPUP");
             }
         }
 
@@ -119,7 +123,8 @@ namespace ItemNamespace
             if (inventory[2] != null)
             {
                 gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[2]);
-                selectedItem.transform.position = sprites[2].transform.position;
+                selectedItem.transform.position = sprites[2].transform.position + new Vector3(0f, 40f, 0f);
+                animator.SetTrigger("itemPOPUP");
             }
         }
 
@@ -128,14 +133,16 @@ namespace ItemNamespace
             if (inventory[3] != null && gameObject.GetComponent<GlobalPlayerInfo>().GetMeatStackNumber() > 0)
             {
                 gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[3]);
-                selectedItem.transform.position = sprites[3].transform.position;
+                selectedItem.transform.position = sprites[3].transform.position + new Vector3(0f, 40f, 0f);
+                animator.SetTrigger("itemPOPUP");
             }
         }
 
         public void ReturnToDefault()
         {
             gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[0]);
-            selectedItem.transform.position = sprites[0].transform.position;
+            selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f, 40f, 0f);
+            animator.SetTrigger("itemPOPUP");
         }
 
         public void UpdateMeatStack()
