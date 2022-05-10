6481de8bba0eba57e46a8d389b3ae87365d20cbf
munchosaurus <martinkings1@live.se>
2022-05-10 15:26:09 +0200

    post conflict fix

--- Viking Attack/Assets/Scripts/Inventory_scripts/PlayerInventory.cs
+++ Viking Attack/Assets/Scripts/Inventory_scripts/PlayerInventory.cs
@@ -14,13 +14,9 @@ namespace ItemNamespace
         [SerializeField] private Guid itemPickupGuid;
         [SerializeField] private GameObject selectedItem;
         [SerializeField] private GameObject meatStackNumber;
-<<<<<<< Updated upstream
         [SerializeField] public Animator animator;
-
-
-=======
         [SerializeField] private uint netID;
->>>>>>> Stashed changes
+
 
         private void Start()
         {
@@ -36,77 +32,10 @@ namespace ItemNamespace
         {
             if (isLocalPlayer)
             {
-<<<<<<< Updated upstream
-                case ItemBase.ItemType.Weapon:
-                    animator.SetTrigger("itemPOPUP");
-                    switch (playerItemPickupEventInfo.itemBase.GetWeaponType)
-                    {
-                        // Sets the inventory slot, sprite updates globalplayerinfo, what item the player is using 
-                        // in each case
-                        case ItemBase.WeaponType.Sword:
-                            inventory[0] = playerItemPickupEventInfo.itemBase;
-                            sprites[0].SetActive(true);
-                            sprites[0].GetComponent<Image>().sprite = inventory[0].GetSprite;
-                            gameObject.GetComponent<GlobalPlayerInfo>()
-                                .SetItemSlot(0, inventory[0]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f, 40f, 0f);
-                            gameObject.GetComponent<PlayerItemUsageController>()
-                                .ChangeItem(playerItemPickupEventInfo.itemBase);
-                            break;
-
-                        case ItemBase.WeaponType.Spear:
-                            inventory[1] = playerItemPickupEventInfo.itemBase;
-                            sprites[1].SetActive(true);
-                            sprites[1].GetComponent<Image>().sprite = inventory[1].GetSprite;
-                            gameObject.GetComponent<GlobalPlayerInfo>()
-                                .SetItemSlot(1, inventory[1]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[1].transform.position + new Vector3(0f, 40f, 0f);
-                            gameObject.GetComponent<PlayerItemUsageController>()
-                                .ChangeItem(playerItemPickupEventInfo.itemBase);
-                            break;
-
-                        case ItemBase.WeaponType.Dagger:
-                            inventory[2] = playerItemPickupEventInfo.itemBase;
-                            sprites[2].SetActive(true);
-                            sprites[2].GetComponent<Image>().sprite = inventory[2].GetSprite;
-                            gameObject.GetComponent<GlobalPlayerInfo>()
-                                .SetItemSlot(2, inventory[2]); // sets the info in globalplayerinfo
-                            selectedItem.transform.position = sprites[2].transform.position + new Vector3(0f, 40f, 0f);
-                            gameObject.GetComponent<PlayerItemUsageController>()
-                                .ChangeItem(playerItemPickupEventInfo.itemBase);
-                            break;
-                    }
-
-                    break;
-                // END OF INNER WEAPON SWITCH
-
-                case ItemBase.ItemType.Food:
-                    if (inventory[3] != null)
-                    {
-                        gameObject.GetComponent<GlobalPlayerInfo>().IncreaseMeatStackNumber();
-                        meatStackNumber.GetComponent<Text>().text =
-                            gameObject.GetComponent<GlobalPlayerInfo>().GetMeatStackNumber().ToString();
-                    }
-                    else
-                    {
-                        gameObject.GetComponent<GlobalPlayerInfo>().IncreaseMeatStackNumber();
-                        inventory[3] = playerItemPickupEventInfo.itemBase;
-                        sprites[3].SetActive(true);
-                        sprites[3].GetComponent<Image>().sprite = inventory[3].GetSprite;
-                        meatStackNumber.GetComponent<Text>().text =
-                            gameObject.GetComponent<GlobalPlayerInfo>().GetMeatStackNumber().ToString();
-                        gameObject.GetComponent<GlobalPlayerInfo>()
-                            .SetItemSlot(3, inventory[3]); // sets the info in globalplayerinfo
-                    }
-                    break;
-                default:
-                    // code block
-                    break;
-=======
                 switch (playerItemPickupEventInfo.itemBase.GetItemType)
                 {
                     case ItemBase.ItemType.Weapon:
-
+                        animator.SetTrigger("itemPOPUP");
                         switch (playerItemPickupEventInfo.itemBase.GetWeaponType)
                         {
                             // Sets the inventory slot, sprite updates globalplayerinfo, what item the player is using 
@@ -117,7 +46,8 @@ namespace ItemNamespace
                                 sprites[0].GetComponent<Image>().sprite = inventory[0].GetSprite;
                                 gameObject.GetComponent<GlobalPlayerInfo>()
                                     .SetItemSlot(0, inventory[0]); // sets the info in globalplayerinfo
-                                selectedItem.transform.position = sprites[0].transform.position;
+                                selectedItem.transform.position =
+                                    sprites[0].transform.position + new Vector3(0f, 40f, 0f);
                                 gameObject.GetComponent<PlayerItemUsageController>()
                                     .ChangeItem(playerItemPickupEventInfo.itemBase);
                                 break;
@@ -128,7 +58,8 @@ namespace ItemNamespace
                                 sprites[1].GetComponent<Image>().sprite = inventory[1].GetSprite;
                                 gameObject.GetComponent<GlobalPlayerInfo>()
                                     .SetItemSlot(1, inventory[1]); // sets the info in globalplayerinfo
-                                selectedItem.transform.position = sprites[1].transform.position;
+                                selectedItem.transform.position =
+                                    sprites[1].transform.position + new Vector3(0f, 40f, 0f);
                                 gameObject.GetComponent<PlayerItemUsageController>()
                                     .ChangeItem(playerItemPickupEventInfo.itemBase);
                                 break;
@@ -139,7 +70,8 @@ namespace ItemNamespace
                                 sprites[2].GetComponent<Image>().sprite = inventory[2].GetSprite;
                                 gameObject.GetComponent<GlobalPlayerInfo>()
                                     .SetItemSlot(2, inventory[2]); // sets the info in globalplayerinfo
-                                selectedItem.transform.position = sprites[2].transform.position;
+                                selectedItem.transform.position =
+                                    sprites[2].transform.position + new Vector3(0f, 40f, 0f);
                                 gameObject.GetComponent<PlayerItemUsageController>()
                                     .ChangeItem(playerItemPickupEventInfo.itemBase);
                                 break;
@@ -188,7 +120,6 @@ namespace ItemNamespace
                         // code block
                         break;
                 }
->>>>>>> Stashed changes
             }
         }
 
@@ -204,12 +135,9 @@ namespace ItemNamespace
             }
             else
             {
-<<<<<<< Updated upstream
                 gameObject.GetComponent<PlayerItemUsageController>().ChangeItem(inventory[0]);
-                selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f,40f,0f);
+                selectedItem.transform.position = sprites[0].transform.position + new Vector3(0f, 40f, 0f);
                 animator.SetTrigger("itemPOPUP");
-=======
->>>>>>> Stashed changes
             }
         }
 
