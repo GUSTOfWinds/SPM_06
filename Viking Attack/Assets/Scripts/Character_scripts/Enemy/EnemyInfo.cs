ad750f5852425a5f3795860fd32f4fa959b9dd67
fionaliu66 <fiona.liu0606@gmail.com>
2022-05-10 11:30:13 +0200

    Revert "Merge branch 'Jiang_Test'"
    This reverts commit 6ddccf227f4a80dda2902f277686a561902974a8, reversing
    changes made to c582e9c7edeec19c13806095f6769c44b08c765c.

--- Viking Attack/Assets/Scripts/Character_scripts/Enemy/EnemyInfo.cs
+++ Viking Attack/Assets/Scripts/Character_scripts/Enemy/EnemyInfo.cs
@@ -87,12 +87,5 @@ namespace ItemNamespace
                 gameObject.GetComponent<EnemyInfo>().Kill();
             }
         }
-
-        //get health back when moving back to default status
-        //public void BackToDefault(GameObject chasingObj)
-        //{
-        //    this.gameObject.GetComponent<EnemyInfo>().health = maxHealth;
-        //    chasingObj.transform.Find("Health_bar").gameObject.GetComponent<EnemyHealthBar>().SetHealth();
-        //}
     }
 }
