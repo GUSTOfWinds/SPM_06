8bd586dbbdc0081951b4a58aba096e39e21ccf99
ryokz <82468014+ryokz@users.noreply.github.com>
2022-05-12 21:43:12 +0200

    Fixade buggar när det kommer till map

--- Viking Attack/Assets/Scripts/MovePlayerOnMap.cs
+++ Viking Attack/Assets/Scripts/MovePlayerOnMap.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using TMPro;
 
-public class MovePlayerOnMap : MonoBehaviour
+public class MovePlayerOnMap : NetworkBehaviour
 {
     private GameObject player;
     private RectTransform rectTransform;
+    private TextMeshProUGUI text;
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        text = GetComponent<TextMeshProUGUI>();
     }
     void Update()
     {
-        if(player != null)
-            rectTransform.anchoredPosition3D = new Vector3(player.gameObject.transform.position.x,player.gameObject.transform.position.z,-1)*3f;
-        else
-            player = GameObject.FindGameObjectWithTag("Player");
+        if(!isLocalPlayer)
+            if(player != null)
+            {
+                rectTransform.anchoredPosition3D = new Vector3(player.gameObject.transform.position.x,player.gameObject.transform.position.z,-1)*3f;
+            }   
+            else
+            {
+                player = GameObject.FindGameObjectWithTag("Player");
+                if(player != null)
+                    text.text = (player.GetComponent<GlobalPlayerInfo>().GetName());
+            }
+                
     }
 }
