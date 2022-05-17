bc40c6b80f07884e97fff8d90194ccb2d8840f6c
baltzius <71239999+baltzius@users.noreply.github.com>
2022-05-17 16:23:42 +0200

    Update TextColourChanger.cs

--- Viking Attack/Assets/Scenes/Colour/TextColourChanger.cs
+++ Viking Attack/Assets/Scenes/Colour/TextColourChanger.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +15,6 @@ namespace Scenes.Colour
         private float redVal;
         private float blueVal;
         private float greenVal;
-        private Color32 color32;
 
 
         /*
@@ -23,37 +23,18 @@ namespace Scenes.Colour
             GameObject.FindGameObjectWithTag("RSlider");
             GameObject.FindGameObjectWithTag("GSlider");
             GameObject.FindGameObjectWithTag("BSlider");
-            color32 = GetColour();
 
         }
         */
 
-        private Color32 GetColour()
-        {
-            byte r = (byte)PlayerPrefs.GetInt("redValue");
-            byte g = (byte)PlayerPrefs.GetInt("greenValue");
-            byte b = (byte)PlayerPrefs.GetInt("blueValue");
-            return new Color32(r, g, b, 255);
-        }
-
         
         private void FixedUpdate()
         {
-            
             redVal = redSlider.value;
             blueVal = blueSlider.value;
             greenVal = greenSlider.value;
-            color32 = new Color32((byte)redVal, (byte)greenVal, (byte)blueVal, 255);
-            mixedColour.color = color32;
-            SaveColour();
-
-        }
-
-        private void SaveColour()
-        {
-            PlayerPrefs.SetInt("redValue", (int)redVal);
-            PlayerPrefs.GetInt("greenValue", (int) greenVal);
-            PlayerPrefs.GetInt("blueValue", (int) blueVal);
+            mixedColour.color = new Color32((byte)redVal, (byte)greenVal, (byte)blueVal, 255);
+            
         }
 
         public void UpdateOnChange()
