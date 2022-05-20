f759e006222feef3399e03035ef9f8105092b3a8
munchosaurus <martinkings1@live.se>
2022-05-20 13:40:58 +0200

    Updated portal

--- Viking Attack/Assets/Scripts/GoalTextScript.cs
+++ Viking Attack/Assets/Scripts/GoalTextScript.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class GoalTextScript : MonoBehaviour
 {
+
+    [SerializeField] private TMP_Text textOne;
+    [SerializeField] private TMP_Text textTwo;
     void Start()
     {
-        StartCoroutine(Wait(20));
+        textOne.text = "Find your way to the portal to progress to the second level";
+        textTwo.text = "Along the way you will encounter enemies protecting the treasure guardian";
+        StartCoroutine(Wait(5));
     }
 
     private IEnumerator Wait(float time)
     {
         yield return new WaitForSeconds(time);
-        Destroy(gameObject);
+        Debug.Log("yesboi");
+        gameObject.SetActive(false);
+    }
+
+    public void UpdateTextAndDisplay(string one, string two)
+    {
+        textOne.text = one;
+        textTwo.text = two;
+    }
+
+    public string GetTextOne()
+    {
+        return textOne.text;
+    }
+    
+    public string GetTextTwo()
+    {
+        return textTwo.text;
     }
 
 }
