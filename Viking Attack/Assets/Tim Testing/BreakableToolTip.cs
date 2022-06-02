410ba3ce8a6e8ab5a34f77a341690ce063a605dc
Razerclaws <slutbacktim@gmail.com>
2022-06-02 11:26:25 +0200

    breakableinfo

--- Viking Attack/Assets/Tim Testing/BreakableToolTip.cs
+++ Viking Attack/Assets/Tim Testing/BreakableToolTip.cs
@@ -1,26 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Event;
+using ItemNamespace;
+using Mirror;
 using UnityEngine;
-using TMPro;
 
 public class BreakableToolTip : MonoBehaviour
 {
     [SerializeField] private GameObject[] players;
-    [SerializeField] private TMP_Text textOne;
+    [SerializeField] private string text;
+    private GameObject tipText;
+
     // Start is called before the first frame update
     void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
-
     }
     void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") == false)
+        {
             return;
+        }
+        tipText = other.transform.Find("UI").gameObject.transform.Find("TipBreakable").gameObject;
+        if (tipText.active == false)
+        {
+            tipText.SetActive(true);
+            
+        }
         
+
     }
-        // Update is called once per frame
-        void Update()
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") == false)
+        {
+            return;
+        }
+        tipText = other.transform.Find("UI").gameObject.transform.Find("TipBreakable").gameObject;
+        tipText.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
     {
         
     }
